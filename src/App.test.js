3d312c1cfed11f6401770789149642558280e0e7
Juha Hinkula <hinkula@gmail.com>
2022-10-06 12:30:16 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -10,24 +10,3 @@ test('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-test('renders todotable', () => {
-  const row = [{desc: 'Go to coffee', date: '24.01.2021'}];  
-  render(<TodoTable todos={row} />);
-  const tablecell = screen.getByText(/go to coffee/i);  
-  expect(tablecell).toBeInTheDocument();
-});
-
-test('add & clear todo',() => {  
-  render(<App />);
-  // Add todo
-  const desc = screen.getByPlaceholderText('Description');
-  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
-  const date = screen.getByPlaceholderText('Date');   
-  fireEvent.change(date, { target: { value: '29.01.2021' } })
-  fireEvent.click(screen.getByText('Add'));
-  expect(screen.getByText(/go to coffee/i)).toBeInTheDocument();
-  // Clear todo
-  fireEvent.click(screen.getByText('Clear'));
-  expect(screen.queryByText(/go to coffee/i)).not.toBeInTheDocument();
-})
