6b1aa17d5892e39c18001211f1cae8c47ba45ea8
Juha Hinkula <hinkula@gmail.com>
2022-10-17 14:04:44 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import TodoTable from './TodoTable';
-import { render, screen, fireEvent } from'@testing-library/react';
-import'@testing-library/jest-dom/extend-expect';
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
